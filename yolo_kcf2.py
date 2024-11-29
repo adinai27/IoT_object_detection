63ae82fe8df0335a4b51fadd3f219b99b38b8df3
Adinai Akylova <152869699+adinai27@users.noreply.github.com>
2024-11-29 21:04:48 +0900

    Update yolo_kcf2.py

--- yolo_kcf2.py
+++ yolo_kcf2.py
@@ -5,7 +5,7 @@ import cv2
 model = torch.hub.load('ultralytics/yolov5', 'yolov5m')
 
 # Initialize video
-cap = cv2.VideoCapture('/Users/irmuun/Documents/Aya_IOT/YoloTracking-main/highway360.mp4')
+cap = cv2.VideoCapture('/Users/adinai/Downloads/Aya_IOT/YoloTracking-main/highway360.mp4 ')
 
 # Initialize tracker for each car detected
 trackers = []
